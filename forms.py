cd2ef109e1a81a3a46b5c6451158d37748cbf342
vvduth <ducthai060501@gmail.com>
2025-02-06 10:08:08 +0200

    Add user authentication templates and forms

---
+++ forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+
+
+
+# TODO: Create a RegisterForm to register new users
+
+
+# TODO: Create a LoginForm to login existing users
+
+
+# TODO: Create a CommentForm so users can leave comments below posts
